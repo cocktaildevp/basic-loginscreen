6f7eccfc20fa919db1845d3b9c06689a462c6a98
Yiğit Özdemir <87499077+cocktaildevp@users.noreply.github.com>
2023-03-18 22:38:06 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $host = "localhost";  
     $user = "root";  
     $password = '';  
-    $db_name = "xyzchecker";  
+    $db_name = "xyzdb";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
